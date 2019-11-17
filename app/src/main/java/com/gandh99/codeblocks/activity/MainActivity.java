22f64248c1459707c1017818260a2fe7c11cdd79
Gan Ding Han <dhg18@ic.ac.uk>
2019-11-17 17:55:23 +0800

    Added registration page

--- app/src/main/java/com/gandh99/codeblocks/activity/MainActivity.java
+++ app/src/main/java/com/gandh99/codeblocks/activity/MainActivity.java
@@ -3,17 +3,43 @@ package com.gandh99.codeblocks.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.gandh99.codeblocks.R;
 import com.gandh99.codeblocks.fragment.LoginFragment;
+import com.gandh99.codeblocks.fragment.RegisterFragment;
 
 public class MainActivity extends AppCompatActivity {
+  private Button buttonLoginRegister;
+  private State currentState = State.LOGIN;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    buttonLoginRegister = findViewById(R.id.button_login_register);
+
+    initLoginRegisterButton();
     startLoginFragment();
+  }
+
+  private void initLoginRegisterButton() {
+    buttonLoginRegister.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        if (currentState == State.LOGIN) {
+          startRegisterFragment();
+          buttonLoginRegister.setText(State.REGISTER.message);
+          currentState = State.REGISTER;
+        } else {
+          startLoginFragment();
+          buttonLoginRegister.setText(State.LOGIN.message);
+          currentState = State.LOGIN;
+        }
+      }
+    });
   }
 
   private void startLoginFragment() {
@@ -21,5 +47,23 @@ public class MainActivity extends AppCompatActivity {
       .beginTransaction()
       .replace(R.id.main_fragment_container, new LoginFragment())
       .commit();
+  }
+
+  private void startRegisterFragment() {
+    getSupportFragmentManager()
+      .beginTransaction()
+      .replace(R.id.main_fragment_container, new RegisterFragment())
+      .commit();
+  }
+
+  private enum State {
+    LOGIN("New around here? Tap to register"),
+    REGISTER("Already have an account? Tap to login");
+
+    private String message;
+
+    State(String message) {
+      this.message = message;
+    }
   }
 }
--- app/src/main/java/com/gandh99/codeblocks/di/module/FragmentModule.java
+++ app/src/main/java/com/gandh99/codeblocks/di/module/FragmentModule.java
@@ -1,10 +1,16 @@
 package com.gandh99.codeblocks.di.module;
 
+import com.gandh99.codeblocks.fragment.LoginFragment;
+import com.gandh99.codeblocks.fragment.RegisterFragment;
+
 import dagger.Module;
 import dagger.android.ContributesAndroidInjector;
 
 @Module
 public abstract class FragmentModule {
-//  @ContributesAndroidInjector
-//  abstract FragmentExample contributeFragmentExample();
+  @ContributesAndroidInjector
+  abstract LoginFragment contributeLoginFragment();
+
+  @ContributesAndroidInjector
+  abstract RegisterFragment contributeRegisterFragment();
 }
