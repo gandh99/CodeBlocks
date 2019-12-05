18105c7318d3aee92939fd3b91860787f6beb772
Gan Ding Han <dhg18@ic.ac.uk>
2019-12-05 21:19:19 +0800

    Implemented functionality to save the state of the selected radio buttons in each radio group in the SortTaskDialog

--- app/src/main/java/com/gandh99/codeblocks/projectPage/tasks/SortTaskDialog.java
+++ app/src/main/java/com/gandh99/codeblocks/projectPage/tasks/SortTaskDialog.java
@@ -1,14 +1,19 @@
 package com.gandh99.codeblocks.projectPage.tasks;
 
 import android.app.Dialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Point;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -25,8 +30,16 @@ import javax.inject.Inject;
 import dagger.android.support.AndroidSupportInjection;
 
 public class SortTaskDialog extends DialogFragment {
+  public static final String SORT_TASK_DIALOG_PREFERENCES =
+    "com.gandh99.codeblocks.projectPage.tasks.SortTaskDialog";
+  public static final String SORT_BY_PREFERENCE = "sortBy";
+  public static final String ORDER_PREFERENCE = "order";
+
+  private SharedPreferences sharedPreferences;
   private Button buttonSave, buttonCancel;
   private TaskViewModel taskViewModel;
+  private RadioGroup radioGroupSortBy, radioGroupOrder;
+  private int selectedSortById, selectedOrderId;
 
   @Inject
   ViewModelProvider.Factory viewModelFactory;
@@ -45,10 +58,15 @@ public class SortTaskDialog extends DialogFragment {
       .inflate(R.layout.dialog_sort_task, null);
 
     // Get the view items
+    radioGroupSortBy = view.findViewById(R.id.radioGroup_sort_by);
+    radioGroupOrder = view.findViewById(R.id.radioGroup_order);
     buttonSave = view.findViewById(R.id.button_sort_task_save);
     buttonCancel = view.findViewById(R.id.button_sort_task_cancel);
 
     initViewModel();
+    initRadioButtons();
+    initSaveButton();
+    initCancelButton();
 
     return new AlertDialog.Builder(getActivity())
       .setView(view)
@@ -57,6 +75,50 @@ public class SortTaskDialog extends DialogFragment {
 
   private void initViewModel() {
     taskViewModel = ViewModelProviders.of(this, viewModelFactory).get(TaskViewModel.class);
+  }
+
+  private void initRadioButtons() {
+    try {
+      sharedPreferences = getContext().getSharedPreferences(SORT_TASK_DIALOG_PREFERENCES, Context.MODE_PRIVATE);
+
+      // Restore the state of the checked radio buttons in each RadioGroup
+      selectedSortById = sharedPreferences.getInt(SORT_BY_PREFERENCE, -1);
+      selectedOrderId = sharedPreferences.getInt(ORDER_PREFERENCE, -1);
+      radioGroupSortBy.check(selectedSortById);
+      radioGroupOrder.check(selectedOrderId);
+    } catch (NullPointerException e) {
+    }
+  }
+
+  private void initSaveButton() {
+    buttonSave.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        selectedSortById = radioGroupSortBy.getCheckedRadioButtonId();
+        selectedOrderId = radioGroupOrder.getCheckedRadioButtonId();
+        
+        saveRadioButtons();
+        dismiss();
+      }
+    });
+  }
+
+  private void saveRadioButtons() {
+    // Save the state of the checked radio buttons in their respective RadioGroup
+    sharedPreferences = getContext().getSharedPreferences(SORT_TASK_DIALOG_PREFERENCES, Context.MODE_PRIVATE);
+    SharedPreferences.Editor editor = sharedPreferences.edit();
+    editor.putInt(SORT_BY_PREFERENCE, selectedSortById);
+    editor.putInt(ORDER_PREFERENCE, selectedOrderId);
+    editor.apply();
+  }
+
+  private void initCancelButton() {
+    buttonCancel.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        dismiss();
+      }
+    });
   }
 
   @Override
