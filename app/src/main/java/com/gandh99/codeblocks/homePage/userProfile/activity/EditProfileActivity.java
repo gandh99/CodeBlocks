278904dbfce2014cea47b34264bce4df37de4476
Gan Ding Han <dhg18@ic.ac.uk>
2019-12-08 10:55:51 +0800

    Implemented some functionality to save the user profile. However, it is not yet linked up to the Django server

--- app/src/main/java/com/gandh99/codeblocks/homePage/userProfile/activity/EditProfileActivity.java
+++ app/src/main/java/com/gandh99/codeblocks/homePage/userProfile/activity/EditProfileActivity.java
@@ -6,8 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.gandh99.codeblocks.R;
+import com.gandh99.codeblocks.authentication.InputValidator;
+
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjection;
+import dagger.android.support.AndroidSupportInjection;
 
 public class EditProfileActivity extends AppCompatActivity {
   public static final String LOCATION_INTENT = "location";
@@ -17,21 +24,29 @@ public class EditProfileActivity extends AppCompatActivity {
   public static final String WEBSITE_INTENT = "website";
   public static final String PERSONAL_MESSAGE_INTENT = "personalMessage";
   public static final int EDIT_PROFILE_REQUEST_CODE = 1;
-  
+
   private String location, company, jobTitle, email, website, personalMessage;
   private EditText editTextLocation, editTextCompany, editTextJobTitle, editTextEmail,
     editTextWebsite, editTextPersonalMessage;
   private Button buttonSaveProfile;
-  
+
+  @Inject
+  InputValidator inputValidator;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_edit_profile);
-    
+
+    initDagger();
     initToolbar();
     getIntentData();
     initViews();
+    initSaveProfileButton();
+  }
 
+  private void initDagger() {
+    AndroidInjection.inject(this);
   }
 
   private void getIntentData() {
@@ -64,6 +79,37 @@ public class EditProfileActivity extends AppCompatActivity {
     editTextEmail.setText(email);
     editTextWebsite.setText(website);
     editTextPersonalMessage.setText(personalMessage);
+  }
+
+  private void initSaveProfileButton() {
+    buttonSaveProfile.setOnClickListener(view -> {
+      location = editTextLocation.getText().toString();
+      company = editTextCompany.getText().toString();
+      jobTitle = editTextJobTitle.getText().toString();
+      email = editTextEmail.getText().toString();
+      website = editTextWebsite.getText().toString();
+      personalMessage = editTextPersonalMessage.getText().toString();
+
+      if (inputValidator.isInvalidInput(location)
+        || inputValidator.isInvalidInput(company)
+        || inputValidator.isInvalidInput(jobTitle)
+        || inputValidator.isInvalidInput(email)
+        || inputValidator.isInvalidInput(website)
+        || inputValidator.isInvalidInput(personalMessage)) {
+        Toast.makeText(this, "Please enter all fields", Toast.LENGTH_SHORT).show();
+        return;
+      }
+
+      Intent returnIntent = new Intent();
+      returnIntent.putExtra(LOCATION_INTENT, location);
+      returnIntent.putExtra(COMPANY_INTENT, company);
+      returnIntent.putExtra(JOB_TITLE_INTENT, jobTitle);
+      returnIntent.putExtra(EMAIL_INTENT, email);
+      returnIntent.putExtra(WEBSITE_INTENT, website);
+      returnIntent.putExtra(PERSONAL_MESSAGE_INTENT, personalMessage);
+      setResult(RESULT_OK, returnIntent);
+      finish();
+    });
   }
 
   @Override
